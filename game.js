608ff74033bda4fe69cf1ba4ce2b0b6b8025e18a
MikeFlexor <53556444+MikeFlexor@users.noreply.github.com>
2019-12-14 12:46:04 +0300

    Update game.js

--- game.js
+++ game.js
@@ -461,7 +461,7 @@ function setup() {
 	
 	for (let i = 0; i < 16; i++) {
 		let playerRun = PIXI.loader.resources['images/PlayerRunAnim/Run_' + (i) + '.png'].texture;
-		//playerRun.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
+		playerRun.baseTexture.scaleMode = PIXI.SCALE_MODES.LINEAR;
 		playerRunTextures.push(playerRun);
 	}
 	
