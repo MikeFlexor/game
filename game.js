d8be0702b91df0627ce62fa065e4f764e07ee82b
MikeFlexor <53556444+MikeFlexor@users.noreply.github.com>
2019-12-24 15:07:12 +0300

    Update game.js

--- game.js
+++ game.js
@@ -354,7 +354,7 @@ function setup() {
 	player.sprite.y = player.y;
 	player.sprite.width = 46;
 	player.sprite.height = 48;
-	player.scale = player.sprite.width / 485;
+	player.scale = 1;
 	app.stage.addChild(player.sprite);
 	
 	menuAmmo = new PIXI.Sprite(PIXI.loader.resources["images/menu_ammo.png"].texture);
